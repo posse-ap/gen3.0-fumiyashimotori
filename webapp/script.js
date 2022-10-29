a0f68034dd20db6226e66bd8dbaef309ec6b5690
fumiyashimotori <0et52x402pj189m@au.com.co.jp>
2022-10-29 12:01:54 +0900

    twitter 投稿機能他

--- webapp/script.js
+++ webapp/script.js
@@ -12,33 +12,46 @@ $closeButton.click((e) => {
   window.location.reload();
 })
 
+//  Twitterボタン
+//openTwitter(投稿文、シェアするURL、ハッシュタグ、提供元アカウント)
+
+const write_down = document.getElementById('twitter');
+
+function openTwitter() {
+  const $get_text = document.getElementById("textarea").value;
+  const turl = `http://twitter.com/intent/tweet?&text=`+ $get_text
+  window.open(turl,'_blank');
+}
+
+// twitterにシェアするを押したら投稿
+// openTwitter("テスト","https://santmove.com","santmove","santmove_com");
 // 投稿ボタン押下時のぐるぐる画面と投稿完了画面
 const $loading = $('.loading-wrapper');
 const $submit_button = $('.submit-btn');
 const $circle = $('.circle');
 const $done = $('.done');
 
+
 $submit_button.click((e) => {
+  if(write_down.checked){
+   
+    openTwitter()
+  }
   // モーダルの画面を閉じる
   $container.addClass('vanish');
   // 入れ替わりでローディングを表示
+  
   $loading.addClass('openLoading');
   // 3秒で閉じる
     setTimeout(() => {
       $done.addClass('openDone');
     },3000);
-    
-  
-    // window.location.reload();
   });
-  
-//  Twitterボタン
-//openTwitter(投稿文、シェアするURL、ハッシュタグ、提供元アカウント)
-function openTwitter(text,url,hash,account) {
-	var turl = "https://twitter.com/intent/tweet?text="+text+"&url="+url+"&hashtags="+hash+"&via="+account;
-	window.open(turl,'_blank');
-}
-//呼び出し例：openTwitter("テスト","https://santmove.com","santmove","santmove_com");
+
+
+
+
+
 
 
 // 日付
@@ -67,58 +80,75 @@ var ctx = document.getElementById('myChart-time').getContext('2d');
         data: {
             labels: ['1','2', '3', '4', '5','6', '7','8', '10','11','12', '13', '14', '15', '16', '17', '18',  '19','20','21',  '22', '23', '24', '25', '26', '27','28', '29', '30', ],
             datasets: [{
-                label: 'マイグラフ',
-                data: [2.5, 8, 5, 2, 2.0, 3.0, 4.5, 1.0, 5, 2, 2.0, 3.0,2.5, 1.0, 5, 2, 2.0, 3.0, 4.5, 1.0, 5, 2, 2.0, 3.0,5, 2, 2.0, 3.0,],
+                
+                data: [2.5, 8, 5, 2, 2.0, 3.0, 4.5, 1.0, 5, 2, 2.0, 3.0,2.5, 1.0, 5, 2, 2.0, 3.0, 4.5, 1.0, 5, 2, 2.0, 3.0,5, 2, 2.0, 3.0,7],
                 backgroundColor: 'rgb(30, 144, 255)',
                 borderColor: 'rgb(255, 99, 132)'
-            }]
-        },
-        options:{
-          scales: {
-              xAxes: [{
-                  display: true,                 
-
-              }],
-              yAxes: [{  
-                  display: true,  
-                  ticks : {
-                    callback: function(tick) {
-                      return tick.toString() + '年';
+            }],
+            
+          },
+            options:{
+              scales: {
+               
+                x:{
+                  grid:{
+                    drawOnChartArea:false,
+                  },
+                },
+                y:{
+                  grid:{
+                    drawOnChartArea:false,
+                  },
+                  ticks:{
+                    callback: function(tick){
+                      return tick.toString()+'h';
                     }
                   }
-              }],
-          },
-         
-      }
-      });
+                },
 
+              },
+             
+          },
+      });
+        
 // 円グラフ左
 var ctx = document.getElementById('myChart-lng').getContext('2d');
 
 new Chart(ctx, {
   type: 'doughnut',
   data: {
+    labels:["HTML","CSS","JavaScript","PHP","Laravel","SQL","SHELL","情報システム基礎"],
     datasets: [{
       data: [42,18,10,8,8,7,4,3],
       backgroundColor: ['#342de6','#245b7e','#61a3cc','#9ed7fa','#dea4c2','#7e4d7d','#6935a9','#462974',],
       borderWidth: 0,
     }]
   },
-  options: {   
-    plugins: {
-        data: {
-            color: '#000',
-            font: {
-                weight: 'bold',
-                size: 20,
-            },
-            formatter: (value,ctx) => {
-                return value + '%';
-            }
-        }
+  options: { 
+    labels:{
+     boxwith:10,
     },
-}
-
+    plugins: {
+      datalabels: {
+        color: ["#fff","#fff","#fff",'#9ed7fa','#dea4c2','#7e4d7d','#6935a9','#462974',],
+        font: {
+          weight: "bold",
+          size: 20,
+        },   
+        formatter: (value, ctx) => {
+            return value+'%';
+            },
+      },
+        legend:{
+          position:"bottom",
+          pointStyle:"circle",
+          
+        },       
+    },
+},
+plugins: [
+  ChartDataLabels,
+],
 })
 
 
@@ -129,9 +159,32 @@ new Chart(ctx, {
   type: 'doughnut',
   data: {
     datasets: [{
+      label:[],
       data: [42,33,25,],
       backgroundColor: ['#342de6','#245b7e','#61a3cc',],
       borderWidth: 0
     }]
-  }
+  },
+  options: { 
+    
+    plugins: {
+      datalabels: {
+        color: "#fff",
+        font: {
+          weight: "bold",
+          size: 20,
+        },   
+        formatter: (value, ctx) => {
+            return value+'%';
+            },
+      },
+        legend:{
+          position:"bottom",
+          poinStyle:"circle",
+        },       
+    },
+},
+plugins: [
+  ChartDataLabels,
+],
 })
