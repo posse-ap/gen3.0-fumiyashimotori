10dfc96acb1f204091488e3c104b77ad9cfb404b
fumiyashimotori <0et52x402pj189m@au.com.co.jp>
2023-03-15 23:00:29 +0900

    作成

---
+++ webapp/mysql/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE dates (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    date date,
+);
+
+CREATE TABLE laungages (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    laungage varchar(255),
+);
+
+CREATE TABLE howlong (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    time int,
+);
+
+CREATE TABLE contents (
+    id int AUTO_INCREMENT PRIMARY KEY,
+    content varchar(255),
+);
+
